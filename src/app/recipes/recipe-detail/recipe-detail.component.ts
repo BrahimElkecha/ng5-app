226c83ef5ed76af13ccb6976ab14ed7bad29c0e2
BrahimELKECHA <brahim.e@fassinet.fr>
2018-04-10 17:36:54 +0100

    hooks

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.scss']
 })
-export class RecipeDetailComponent implements OnInit {
+
+export class RecipeDetailComponent implements OnInit, OnChanges {
 	@Input() recipe: Recipe;
+	@Input() recipeInfo: Recipe;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes) {
+  	console.log('Im changed:' +changes.recipe.currentValue.name);
+  }
 }
